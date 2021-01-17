8c133229677336316f4d76f21ef01fbe2980c75b
AJ Cruz <ajcruise.007@gmail.com>
2021-01-17 12:47:35 +0530

    create basic layout of homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <div className="menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span  className ="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Trousers</h1>
+            <span  className ="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Shirts</h1>
+            <span  className ="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+        <div className="content">
+            <h1 className="title">Sneakers</h1>
+            <span className ="subtitle">Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+        <div className="content">
+            <h1 className="title">Gloves</h1>
+            <span  className ="subtitle">Shop Now</span>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
